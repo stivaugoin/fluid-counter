8f9ae49029f68882e2e1fd5a2afb2621557627a4
Steve <stiv.aw@gmail.com>
2015-08-15 16:00:40 -0400

    v1.0.0

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
 	name: 'stivaugoin:fluid-counter',
-	version: '0.0.1',
+	version: '1.0.0',
 	summary: 'Counters stored in MongoDB',
 	git: 'https://github.com/stivaugoin/meteor-fluid-counter.git',
 	documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.0');
+	api.versionsFrom('METEOR@1.0');
 
 	api.use(['mongo-livedata']);
 
